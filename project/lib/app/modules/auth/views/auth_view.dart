787ccbaf7e63246b03a034b1c36e4cc52279816c
Krishna Adhikari <krishna.forkhaki@gmail.com>
2022-10-01 14:49:46 +0545

    project code added

---
+++ project/lib/app/modules/auth/views/auth_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/auth_controller.dart';
+
+class AuthView extends GetView<AuthController> {
+  const AuthView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
